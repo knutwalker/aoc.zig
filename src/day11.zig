9e17026694bee1165a5516a6856d89b110f113ca
Paul Horn <dev@knutwalker.engineer>
2024-12-12 01:35:11 +0100

    day11

--- src/day11.zig
+++ src/day11.zig
@@ -38,38 +38,84 @@ pub fn main() !void {
 }
 // }}}
 
-const Parsed = Str;
+const Parsed = []u57;
 
 pub fn parse(input: Str) !Parsed {
-    return input;
+    return try u.tryParse(Parsed, u.mem.trimRight(u8, input, "\n"));
 }
 
-const Output = i64;
+const Output = u64;
+
+var cache = Cache.init(a);
 
 pub fn part1(input: Parsed) !?Output {
-    _ = input; //autofix
-    return null;
+    return solveAll(input, 25);
 }
 
 pub fn part2(input: Parsed) !?Output {
-    _ = input; //autofix
-    return null;
+    return solveAll(input, 75);
+}
+
+fn solveAll(stones: Parsed, iters: u7) Output {
+    cache.ensureTotalCapacity(256 << 10) catch @panic("oom");
+    var out: Output = 0;
+    for (stones) |stone| out += solveCached(.{ .stone = stone, .iter = iters });
+    return out;
+}
+
+const Key = packed struct(u64) {
+    stone: u57,
+    iter: u7,
+
+    inline fn next(self: Key, stone: u57) Key {
+        return .{ .stone = stone, .iter = self.iter - 1 };
+    }
+};
+const Cache = Map(Key, Output);
+
+fn solveCached(key: Key) Output {
+    // can't use gop because fetching the value might
+    // add new items and invalidate the gop pointer
+    const cached = cache.get(key);
+    if (cached) |v| return v;
+
+    const value = solveNew(key);
+    cache.putAssumeCapacityNoClobber(key, value);
+
+    return value;
+}
+
+fn solveNew(key: Key) Output {
+    if (key.iter == 0) return 1;
+    if (key.stone == 0) return solveCached(key.next(1));
+
+    const log = u.math.log10_int(key.stone);
+    if (log % 2 == 1) {
+        const split = u.math.powi(u57, 10, (log + 1) / 2) catch unreachable;
+        const left = key.stone / split;
+        const right = key.stone % split;
+
+        const lv = solveCached(key.next(left));
+        const rv = solveCached(key.next(right));
+        return lv + rv;
+    } else {
+        return solveCached(key.next(key.stone * 2024));
+    }
 }
 
 test "day11 example" {
     const input =
+        \\125 17
         \\
     ;
 
     const in = try parse(input);
-    try std.testing.expectEqual(null, try part1(in));
-    try std.testing.expectEqual(null, try part2(in));
+    try std.testing.expectEqual(55312, try part1(in));
+    try std.testing.expectEqual(65601038650482, try part2(in));
 }
 
 test "day11 input" {
-    if ("remove_this_when_ready".len > 0) return error.SkipZigTest;
-
     const in = try parse(day_input);
-    try std.testing.expectEqual(null, try part1(in));
-    try std.testing.expectEqual(null, try part2(in));
+    try std.testing.expectEqual(204022, try part1(in));
+    try std.testing.expectEqual(241651071960597, try part2(in));
 }
